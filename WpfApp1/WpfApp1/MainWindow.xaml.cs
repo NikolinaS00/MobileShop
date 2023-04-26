143a244b8f3816b9cd83b4ee8193477a28b46868
Nikolina Stojčić <stojcicn00@gmail.com>
2023-04-26 10:58:49 +0200

    start

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.Model;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MainViewModel viewModel;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_Sale(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void Button_Click_Procurement(object sender, RoutedEventArgs e)
+        {
+
+        }private void Button_Click_Supliers(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_Articles(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/Model/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Navigation;
+using WpfApp1.objects;
+
+namespace WpfApp1.Model
+{
+
+	
+
+	internal class MainViewModel : ObservableObject {
+
+        public HomeViewModel HomeVM { get; set; }
+        public ArticlesViewModel ArticleVM { get; set; }
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                onPropertyChanged();
+            }
+        }
+      public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            CurrentView = HomeVM;
+        }
+    }
+}
